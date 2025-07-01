741e0a4b4edc95590f0f34b42e5b25f51831e08e
satojkee <satojkee@gmail.com>
2025-07-01 11:47:01 +0300

    improvement: docstring adjustments

--- autoria_scraper/core/scrapers/__init__.py
+++ autoria_scraper/core/scrapers/__init__.py
@@ -1,11 +1,26 @@
 """This package contains web-scrapers.
 
 Paged scraper:
-    This one is used for obtaining direct links to the listed cars.
+    This one is used for obtaining "direct" links to the listed cars.
     Then, we can use these links to set up `Direct` scraper.
 
 Direct scraper:
-    todo: add description
+    Extracts necessary information from each "direct" link (1 link = 1 car).
+    Yields a collection of `CarParser` instances (each `CarParser` instance
+     can be easily converted to `Car` instance)
+
+    **Usage example**
+
+    ```python
+    direct_scraper = DirectScraper(
+        phone_url=...
+        links=...,
+        batch_size=...
+    )
+
+    async for chunk in direct_scraper.start():
+        print(chunk) # Collection[Optional["CarParser"]]
+    ```
 """
 
 
